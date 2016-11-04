f0404b1bd00ca55f710394e3b5ea260a87bc84f3
Ryan Jones <ryan@system88.com>
2016-11-04 15:54:31 -0600

    initial commit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,35 @@
+# AlchemyCMS Dragonfly configuration.
+#
+# Consider using some kind of caching solution for image processing.
+# For small projects, we have good experience with Rack::Cache.
+#
+# Larger installations should consider using a CDN, start reading
+# http://markevans.github.io/dragonfly/cache/
+#
+# A complete reference can be found at
+# http://markevans.github.io/dragonfly/configuration/
+#
+# Pictures
+#
+Dragonfly.app(:alchemy_pictures).configure do
+  dragonfly_url nil
+  plugin :imagemagick
+  plugin :svg
+  secret '3eb8e21c8443d9853e37b563500012cf7649b66d0a8df106cfeca58aeec57d5c'
+  url_format '/pictures/:job/:name.:ext'
+
+  datastore :file,
+    root_path: Rails.root.join('uploads/pictures').to_s,
+    server_root: Rails.root.join('public'),
+    store_meta: false
+end
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :alchemy_pictures
+
+# Attachments
+Dragonfly.app(:alchemy_attachments).configure do
+  datastore :file,
+    root_path:  Rails.root.join('uploads/attachments').to_s,
+    store_meta: false
+end
